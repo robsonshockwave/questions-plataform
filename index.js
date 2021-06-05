95ec46c5c4a8fc1eedd079497252a9087084acca
Robson <robsonshockwavegithub@gmail.com>
2021-06-05 00:08:25 -0300

    arquivos estáticos

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const app = express();
 
 // Dizendo para o express usar o EJS como View engine
 app.set('view engine', 'ejs');
+// Dizendo que quer utilizar um arquivbo estático
+app.use(express.static('public'));
 
 app.get("/:nome/:lang", (req, res) => {
     let nome = req.params.nome;
